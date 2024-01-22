7eae513b89b43f0c7fa53feda4444a98936c3277
sibo <roi.siboni@msmail.ariel.ac.il>
2024-01-22 18:00:10 +0200

    7767b4f

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
     // Get number from user
     // printf("Enter a number: ");
     // scanf("%d", &num);
-
+    
     // Use functions from libraries
     printf("Is Palindrome: %d\n", isPalindrome(1234321));
     // printf("Count of Digits: %d\n", countDigits(num));
